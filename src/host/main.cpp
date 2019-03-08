05ebdd1b21b1dd828630d1402d51eb54aeda9565
Kirill Kirilenko <kirill@ultracoder.org>
2019-03-08 20:58:45 +0300

    QDarkStyle used. Fixed MSVC build. Fixed MOC'ing.

--- src/host/main.cpp
+++ src/host/main.cpp
@@ -1,10 +1,25 @@
 #include "MainWindow.hpp"
 
 #include <QApplication>
+#include <QDebug>
+#include <QFile>
+#include <QTextStream>
 
 int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
+
+	QFile f(":/qdarkstyle/style.qss");
+	if (!f.exists())
+	{
+		qWarning() << "Unable to set stylesheet, file not found.";
+	}
+	else
+	{
+		f.open(QFile::ReadOnly | QFile::Text);
+		QTextStream ts(&f);
+		a.setStyleSheet(ts.readAll());
+	}
 
 	CuteHost::MainWindow window;
 
